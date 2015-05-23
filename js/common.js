a7064ac3c757d0af6914391e5b0eaf25426ab8e4
凹凸曼 <543484525@qq.com>
2015-05-23 21:22:11 +0800

    add secen

--- js/common.js
+++ js/common.js
@@ -1,33 +1,77 @@
-/**
- * Created by Administrator on 2015/1/13 0013.
- */
-(function(){
 
-    /* 添加用户窗口事件 改变根字号大小 */
+    var mySwiper = new Swiper('.swiper-container', {
+        paginationClickable: true,
+        direction: 'vertical',
+        speed: 1000,
+        spaceBetween: 100,
+        noSwiping:true,
+        parallax:true
+    });
 
-    document.body.addEventListener('touchstart',function(){});
-    function addEvent(obj,type,fn){
-        if(obj.addEventListener){
-            obj.addEventListener(type,fn,false);
-        } else {
-            obj.attachEvent('on'+type,fn);
+    var startX;
+    var setTimeout1;
+    var setTimeout2;
+
+    mySwiper.on('onTouchStart', function (swiper, e) {
+        //mySwiper.lockSwipeToPrev()
+        var startX=e.changedTouches[0].pageY;
+    });
+
+    mySwiper.on('onTouchMove', function (swiper, e) {
+        //mySwiper.lockSwipeToPrev()
+        clearTimeout(setTimeout1);
+        clearTimeout(setTimeout2);
+    });
+
+    mySwiper.on('onTransitionEnd', function (swiper){
+
+        if (swiper.activeIndex == 1) {
+
+            $('.scene02 .block-content').addClass('moveToNextScene');
+            $('.block-content.moveToNextScene').show();
+            $('.scene02 .block-content').show();
+            setTimeout1 = setTimeout(function (){
+                $('.block-content.moveToNextScene').fadeOut(900);
+                setTimeout2 = setTimeout(function (){
+                    mySwiper.slideNext();
+                }, 800);
+            }, 1300);
+
+            $('.page3-text').hide();
+            $('.page3').hide();
         }
-    }
-    function autoSize(){
-        var win_W = $(".container").width();
-            var font = Math.round(win_W / 16);
-            var HTML=document.getElementById('html');
-            if(win_W==360 || win_W==361){
-                font=22;
-                console.log(font);
-            }
-            HTML.style.fontSize = font + 'px';
+
+        if( mySwiper.previousIndex ==2 && swiper.activeIndex == 1){
+            mySwiper.slideTo(0, 800, false);
+            clearTimeout(setTimeout1);
+            clearTimeout(setTimeout2);
+        }
+
+        if (swiper.activeIndex == 0) {
+            $('.scene02 .block-content').removeClass('moveToNextScene');
+            $('.scene02 .block-content').show();
+            clearTimeout(setTimeout1);
+            clearTimeout(setTimeout2);
+        }
+        if (swiper.activeIndex == 2) {
+            $('.scene02 .block-content').removeClass('moveToNextScene');
+            $('.scene02 .block-content').show();
+            clearTimeout(setTimeout1);
+            clearTimeout(setTimeout2);
+            $('.page3-text').fadeIn(400);
+            var page3 = setTimeout(function(){
+                $('.page3').fadeIn(1400);
+            },300)
+            //mySwiper.slideTo(0, 1000, false);
+        }
+
+        if (swiper.activeIndex == 7) {
+            demo.start();
+        }else{
+
+        }
+
+        console.log(swiper.activeIndex)
+    });
 
 
-    }
-    addEvent(window,'load',autoSize);
-    addEvent(window,'resize',autoSize);
-    /*  end 添加用户窗口事件 */
-
-
-})()
